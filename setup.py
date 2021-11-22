de8af716f4e3ffdcdf65b1d4c5f4a384b694c5b9
Alex8695 <39283168+Alex8695@users.noreply.github.com>
2021-11-21 19:34:42 -0500

    Updated Publish Action

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    install_requires=['numpy','math'],  # Optional
+    install_requires=['numpy'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
